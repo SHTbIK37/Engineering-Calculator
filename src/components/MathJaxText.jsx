7c60163b9a6e5d069f685294105a65b1b78668b3
Ivan <ishtykov0@gmail.com>
2023-11-28 17:22:01 +0300

    visual bug fix

--- src/components/MathJaxText.jsx
+++ src/components/MathJaxText.jsx
@@ -1,39 +1,89 @@
 import { MathJax, MathJaxContext } from "better-react-mathjax";
 export function VoltageFormula() {
   return (
-    <>
-      <MathJaxContext>
-        <MathJax>{"Формула напряжения: \\(U=I*R\\)"}</MathJax>
-        <MathJax>{"\\(U\\) - Напряжение"}</MathJax>
-        <MathJax>{"\\(R\\) - Сопротивление"}</MathJax>
-        <MathJax>{"\\(I\\) - Сила тока"}</MathJax>
-      </MathJaxContext>
-    </>
+    <MathJaxContext>
+      <MathJax>{"Формула напряжения: \\(U=I*R\\)"}</MathJax>
+      <MathJax>{"\\(U\\) - Напряжение"}</MathJax>
+      <MathJax>{"\\(R\\) - Сопротивление"}</MathJax>
+      <MathJax>{"\\(I\\) - Сила тока"}</MathJax>
+    </MathJaxContext>
   );
 }
 export function ForceGravityFormula() {
   return (
-    <>
-      <MathJaxContext>
-        <MathJax>{"Формула силы тяжести: \\(F=m*g\\)"}</MathJax>
-        <MathJax>{"\\(F\\) - Сила тяжести"}</MathJax>
-        <MathJax>{"\\(m\\) - Масса тела"}</MathJax>
-        <MathJax>
-          {"\\(g\\) - Гравитационная постоянная = \\(9.80665м/с^2\\)"}
-        </MathJax>
-      </MathJaxContext>
-    </>
+    <MathJaxContext>
+      <MathJax>{"Формула силы тяжести: \\(F=m*g\\)"}</MathJax>
+      <MathJax>{"\\(F\\) - Сила тяжести"}</MathJax>
+      <MathJax>{"\\(m\\) - Масса тела"}</MathJax>
+      <MathJax>
+        {"\\(g\\) - Гравитационная постоянная = \\(9.80665м/с^2\\)"}
+      </MathJax>
+    </MathJaxContext>
   );
 }
 export function SpeedFormula() {
   return (
-    <>
-      <MathJaxContext>
-        <MathJax>{"Формула скорости: \\(v=S/t\\)"}</MathJax>
-        <MathJax>{"\\(v\\) - Скорость"}</MathJax>
-        <MathJax>{"\\(S\\) - Расстояние"}</MathJax>
-        <MathJax>{"\\(t\\) - Время"}</MathJax>
-      </MathJaxContext>
-    </>
+    <MathJaxContext>
+      <MathJax>{"Формула скорости: \\(v=S/t\\)"}</MathJax>
+      <MathJax>{"\\(v\\) - Скорость"}</MathJax>
+      <MathJax>{"\\(S\\) - Расстояние"}</MathJax>
+      <MathJax>{"\\(t\\) - Время"}</MathJax>
+    </MathJaxContext>
+  );
+}
+export function RenderU() {
+  return (
+    <MathJaxContext>
+      <MathJax>{`\\(U\\) - `}</MathJax>
+    </MathJaxContext>
+  );
+}
+export function RenderI() {
+  return (
+    <MathJaxContext>
+      <MathJax>{`\\(I\\) - `}</MathJax>
+    </MathJaxContext>
+  );
+}
+export function RenderR() {
+  return (
+    <MathJaxContext>
+      <MathJax>{`\\(R\\) - `}</MathJax>
+    </MathJaxContext>
+  );
+}
+export function Renderm() {
+  return (
+    <MathJaxContext>
+      <MathJax>{`\\(m\\) - `}</MathJax>
+    </MathJaxContext>
+  );
+}
+export function RenderF() {
+  return (
+    <MathJaxContext>
+      <MathJax>{`\\(F\\) - `}</MathJax>
+    </MathJaxContext>
+  );
+}
+export function RenderS() {
+  return (
+    <MathJaxContext>
+      <MathJax>{`\\(S\\) - `}</MathJax>
+    </MathJaxContext>
+  );
+}
+export function Renderv() {
+  return (
+    <MathJaxContext>
+      <MathJax>{`\\(v\\) - `}</MathJax>
+    </MathJaxContext>
+  );
+}
+export function Rendert() {
+  return (
+    <MathJaxContext>
+      <MathJax>{`\\(t\\) - `}</MathJax>
+    </MathJaxContext>
   );
 }
--- src/components/Variables.jsx
+++ src/components/Variables.jsx
@@ -1,32 +1,31 @@
 import * as calculations from "../utils/calculations";
 import { MathJax, MathJaxContext } from "better-react-mathjax";
+import * as MathJaxChars from "./MathJaxText";
 
 export function VariablesU(props) {
   if (props.letter === "VariablesU") {
     return (
       <div id="Operations">
-        <MathJaxContext>
-          <MathJax>{`\\(R\\) - `}</MathJax>
-          <input type="number" placeholder="Сопротивление Ом" />
-          <select id="Ohm">
-            <option value="0">Ом</option>
-            <option value="-3">мОм</option>
-            <option value="3">КОм</option>
-            <option value="6">МОм</option>
-          </select>
-          <br />
-          *
-          <br />
-          <MathJax>{"\\(I\\) - "}</MathJax>
-          <input type="number" placeholder="Сила тока А" />
-          <select id="A">
-            <option value="0">A</option>
-            <option value="-6">мкA</option>
-            <option value="-3">мA</option>
-            <option value="3">КA</option>
-            <option value="6">МA</option>
-          </select>
-        </MathJaxContext>
+        <MathJaxChars.RenderR />
+        <input type="number" placeholder="Сопротивление Ом" />
+        <select id="Ohm">
+          <option value="0">Ом</option>
+          <option value="-3">мОм</option>
+          <option value="3">КОм</option>
+          <option value="6">МОм</option>
+        </select>
+        <br />
+        *
+        <br />
+        <MathJaxChars.RenderI />
+        <input type="number" placeholder="Сила тока А" />
+        <select id="A">
+          <option value="0">A</option>
+          <option value="-6">мкA</option>
+          <option value="-3">мA</option>
+          <option value="3">КA</option>
+          <option value="6">МA</option>
+        </select>
         <button
           onClick={() => {
             calculations.calcVoltage(props);
@@ -40,28 +39,26 @@ export function VariablesU(props) {
   if (props.letter === "VariablesR") {
     return (
       <div id="Operations">
-        <MathJaxContext>
-          <MathJax>{"\\(U\\) - "}</MathJax>
-          <input type="number" placeholder="Напряжение Вольт" />
-          <select id="U">
-            <option value="0">Вольт</option>
-            <option value="-3">мВольт</option>
-            <option value="3">КВольт</option>
-            <option value="6">МВольт</option>
-          </select>
-          <br />
-          /
-          <br />
-          <MathJax>{"\\(I\\) - "}</MathJax>
-          <input type="number" placeholder="Сила тока А" />
-          <select id="A">
-            <option value="0">A</option>
-            <option value="-6">мкA</option>
-            <option value="-3">мA</option>
-            <option value="3">КA</option>
-            <option value="6">МA</option>
-          </select>
-        </MathJaxContext>
+        <MathJaxChars.RenderU />
+        <input type="number" placeholder="Напряжение Вольт" />
+        <select id="U">
+          <option value="0">Вольт</option>
+          <option value="-3">мВольт</option>
+          <option value="3">КВольт</option>
+          <option value="6">МВольт</option>
+        </select>
+        <br />
+        /
+        <br />
+        <MathJaxChars.RenderI />
+        <input type="number" placeholder="Сила тока А" />
+        <select id="A">
+          <option value="0">A</option>
+          <option value="-6">мкA</option>
+          <option value="-3">мA</option>
+          <option value="3">КA</option>
+          <option value="6">МA</option>
+        </select>
         <button
           onClick={() => {
             calculations.calcOhm(props);
@@ -75,27 +72,25 @@ export function VariablesU(props) {
   if (props.letter === "VariablesI") {
     return (
       <div id="Operations">
-        <MathJaxContext>
-          <MathJax>{"\\(U\\) - "}</MathJax>
-          <input type="number" placeholder="Напряжение Вольт" />
-          <select id="U">
-            <option value="0">Вольт</option>
-            <option value="-3">мВольт</option>
-            <option value="3">КВольт</option>
-            <option value="6">МВольт</option>
-          </select>
-          <br />
-          /
-          <br />
-          <MathJax>{"\\(R\\) - "}</MathJax>
-          <input type="number" placeholder="Сопротивление Ом" />
-          <select id="Ohm">
-            <option value="0">Ом</option>
-            <option value="-3">мОм</option>
-            <option value="3">КОм</option>
-            <option value="6">МОм</option>
-          </select>
-        </MathJaxContext>
+        <MathJaxChars.RenderU />
+        <input type="number" placeholder="Напряжение Вольт" />
+        <select id="U">
+          <option value="0">Вольт</option>
+          <option value="-3">мВольт</option>
+          <option value="3">КВольт</option>
+          <option value="6">МВольт</option>
+        </select>
+        <br />
+        /
+        <br />
+        <MathJaxChars.RenderR />
+        <input type="number" placeholder="Сопротивление Ом" />
+        <select id="Ohm">
+          <option value="0">Ом</option>
+          <option value="-3">мОм</option>
+          <option value="3">КОм</option>
+          <option value="6">МОм</option>
+        </select>
         <button
           onClick={() => {
             calculations.calcI(props);
@@ -111,18 +106,16 @@ export function VariablesF(props) {
   if (props.letter === "VariablesF") {
     return (
       <div id="Operations">
-        <MathJaxContext>
-          <MathJax>{"\\(m\\) - "}</MathJax>
-          <input type="number" placeholder="Масса Кг" />
-          <select id="KG">
-            <option value="0">Килограмм</option>
-            <option value="-3">Грамм</option>
-            <option value="3">Тонна</option>
-          </select>
-          <br />
-          * g
-          <br />
-        </MathJaxContext>
+        <MathJaxChars.Renderm />
+        <input type="number" placeholder="Масса Кг" />
+        <select id="KG">
+          <option value="0">Килограмм</option>
+          <option value="-3">Грамм</option>
+          <option value="3">Тонна</option>
+        </select>
+        <br />
+        * g
+        <br />
         <button
           onClick={() => {
             calculations.calcForceGravity(props);
@@ -136,16 +129,14 @@ export function VariablesF(props) {
   if (props.letter === "VariablesM") {
     return (
       <div id="Operations">
-        <MathJaxContext>
-          <MathJax>{"\\(F\\) - "}</MathJax>
-          <input type="number" placeholder="Сила тяжести Н" />
-          <select id="N">
-            <option value="0">Ньютон</option>
-            <option value="-3">мНьютон</option>
-            <option value="3">КНьютон</option>
-          </select>
-          <br />/ g <br />
-        </MathJaxContext>
+        <MathJaxChars.RenderF />
+        <input type="number" placeholder="Сила тяжести Н" />
+        <select id="N">
+          <option value="0">Ньютон</option>
+          <option value="-3">мНьютон</option>
+          <option value="3">КНьютон</option>
+        </select>
+        <br />/ g <br />
         <button
           onClick={() => {
             calculations.calcWeightG(props);
@@ -162,27 +153,25 @@ export function Variablesv(props) {
   if (props.letter === "Variablesv") {
     return (
       <div id="Operations">
-        <MathJaxContext>
-          <MathJax>{"\\(S\\) - "}</MathJax>
-          <input type="number" placeholder="Расстояние Метры" />
-          <select id="S">
-            <option value="0">Метры</option>
-            <option value="-3">Миллиметры</option>
-            <option value="-2">Сантиметры</option>
-            <option value="3">Километры</option>
-          </select>
-          <br />
-          /
-          <br />
-          <MathJax>{"\\(t\\) - "}</MathJax>
-          <input type="number" placeholder="Время Секунды" />
-          <select id="Time">
-            <option value="0">Секунды</option>
-            <option value="-3">Милисекунды</option>
-            <option value="1">Минуты</option>
-            <option value="2">Час</option>
-          </select>
-        </MathJaxContext>
+        <MathJaxChars.RenderS />
+        <input type="number" placeholder="Расстояние Метры" />
+        <select id="S">
+          <option value="0">Метры</option>
+          <option value="-3">Миллиметры</option>
+          <option value="-2">Сантиметры</option>
+          <option value="3">Километры</option>
+        </select>
+        <br />
+        /
+        <br />
+        <MathJaxChars.Rendert />
+        <input type="number" placeholder="Время Секунды" />
+        <select id="Time">
+          <option value="0">Секунды</option>
+          <option value="-3">Милисекунды</option>
+          <option value="1">Минуты</option>
+          <option value="2">Час</option>
+        </select>
         <button
           onClick={() => {
             calculations.calcSpeed(props);
@@ -196,25 +185,23 @@ export function Variablesv(props) {
   if (props.letter === "VariablesS") {
     return (
       <div id="Operations">
-        <MathJaxContext>
-          <MathJax>{"\\(v\\) - "}</MathJax>
-          <input type="number" placeholder="Скорость м/с" />
-          <select id="v">
-            <option value="0">м/c</option>
-            <option value="3">Км/ч</option>
-          </select>
-          <br />
-          *
-          <br />
-          <MathJax>{"\\(t\\) - "}</MathJax>
-          <input type="number" placeholder="Время Секунды" />
-          <select id="Time">
-            <option value="0">Секунды</option>
-            <option value="-3">Милисекунды</option>
-            <option value="1">Минуты</option>
-            <option value="2">Час</option>
-          </select>
-        </MathJaxContext>
+        <MathJaxChars.Renderv />
+        <input type="number" placeholder="Скорость м/с" />
+        <select id="v">
+          <option value="0">м/c</option>
+          <option value="3">Км/ч</option>
+        </select>
+        <br />
+        *
+        <br />
+        <MathJaxChars.Rendert />
+        <input type="number" placeholder="Время Секунды" />
+        <select id="Time">
+          <option value="0">Секунды</option>
+          <option value="-3">Милисекунды</option>
+          <option value="1">Минуты</option>
+          <option value="2">Час</option>
+        </select>
         <button
           onClick={() => {
             calculations.calcS(props);
@@ -228,25 +215,23 @@ export function Variablesv(props) {
   if (props.letter === "Variablest") {
     return (
       <div id="Operations">
-        <MathJaxContext>
-          <MathJax>{"\\(S\\) - "}</MathJax>
-          <input type="number" placeholder="Расстояние Метры" />
-          <select id="S">
-            <option value="0">Метры</option>
-            <option value="-3">Миллиметры</option>
-            <option value="-2">Сантиметры</option>
-            <option value="3">Километры</option>
-          </select>
-          <br />
-          /
-          <br />
-          <MathJax>{"\\(v\\) - "}</MathJax>
-          <input type="number" placeholder="Скорость м/с" />
-          <select id="v">
-            <option value="0">м/c</option>
-            <option value="3">Км/ч</option>
-          </select>
-        </MathJaxContext>
+        <MathJaxChars.RenderS />
+        <input type="number" placeholder="Расстояние Метры" />
+        <select id="S">
+          <option value="0">Метры</option>
+          <option value="-3">Миллиметры</option>
+          <option value="-2">Сантиметры</option>
+          <option value="3">Километры</option>
+        </select>
+        <br />
+        /
+        <br />
+        <MathJaxChars.Renderv />
+        <input type="number" placeholder="Скорость м/с" />
+        <select id="v">
+          <option value="0">м/c</option>
+          <option value="3">Км/ч</option>
+        </select>
         <button
           onClick={() => {
             calculations.calct(props);
